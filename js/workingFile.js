09fd7b2d559489db9e5b9b605ae888db85db4b9d
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2019-03-03 20:09:09 +0100

    Add files via upload

---
+++ js/workingFile.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+    //intro
+    $.ajax({
+        url: 'https://szymekcendecki.github.io/Audaces-powerfull-next-generation/jsonFiles/heroCreator.json',
+        type: 'GET',
+        dataType: 'json'
+        }).done((data) => { $("#info").empty().append(data.texts[0].intro);
+        }).fail(()=>{ console.log("coś nie bangla..."); });   		
+});
+
+    $("#title, #subTitle").hide();
+    $("#info").on("click", ()=>{
+        $("#info").addClass("hideBlur");
+        setTimeout(()=>{
+            $("#info").remove();
+            $("#title, #subTitle").show().addClass("showBlur");
+        }, 2500);
+    });
+    
+    $("#title, #subTitle").on("click", ()=>{
+        $("#title, #subTitle").removeClass("showBlur");
+        $("#title, #subTitle").addClass("hideBlur");
+        setTimeout(()=>{
+            $("#title, #subTitle").remove();
+        }, 2050);
+    });
+  
+});
